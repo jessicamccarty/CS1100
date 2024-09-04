7886f22f3d5fa3bbd4785d35a044d55b89cb4ba3
student <jessica.mccarty@atlasschool.com>
2024-09-04 16:58:53 +0000

    init

---
+++ day1_example.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+ls -l  file1.txt
+#This searches for a file named file1.txt."
+rm file2.txt
+#This removed a file named file2.txt."
+mkdir Week_2
+#This creates a directory named Week_2."
+
+
+
+
+
---
+++ example.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+ls -l file1.txt | rm file2.txt | mkdir Week_2
+#This is how you pipe the same codes.#
