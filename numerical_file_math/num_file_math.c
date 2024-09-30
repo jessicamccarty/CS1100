57845b6fe1b802ed51a953604a80ed6be8d5df63
Jessica McCarty <jessicaeitr@gmail.com>
2024-09-30 10:37:45 -0500

    numerical_file_math

---
+++ numerical_file_math/num_file_math.c
@@ -0,0 +1,25 @@
+// Filename: num_file_math.c
+// Author: Jessica McCarty
+// Date: September 30, 2024
+// Problem Statement: Create a program that reads integers from an input file, calculates their sum, and write the result to an output file. The program should demonstrate the use of arrays,
+// file input/output, and basic error handling.
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_NUMBERS 100
+
+// Read numbers from file.
+
+int readNumbersFromFile(const char* filename, int numbers[], int* count) {
+
+
+    if (file == NULL) {
+        printf("Error opening input file.\n"); // Exit the program if file can't be opened.
+
+        exit(1);
+    }
+}
+
+
+//INCOMPLETE
